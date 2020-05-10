b1e6ab299c21a6896df0f01725f6ac60642d8810
Foti.Mougos <foti.mougos@gmail.com>
2020-05-10 23:08:32 +1000

    heroku db added

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,10 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "mysql://bd0d580252c652:f8fdad72@us-cdbr-east-06.cleardb.net/heroku_c3fff59474658c1?reconnect=true",
   port: 3306,
-  user: "root",
-  password: "FogusMogus",
+  user: "bd0d580252c652",
+  password: "f8fdad72",
   database: "burgers_db"
 });
 
