a13a306eeb8903ad41589f9571f217ab7d8a6d06
Foti.Mougos <foti.mougos@gmail.com>
2020-05-12 18:23:09 +1000

    fixed db

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,15 @@
 
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "mysql://bd0d580252c652:f8fdad72@us-cdbr-east-06.cleardb.net/heroku_c3fff59474658c1?reconnect=true",
-  port: 3306,
-  user: "bd0d580252c652",
-  password: "f8fdad72",
-  database: "burgers_db"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
+if (process.env.JAWSDB_URL) {
+  connection = mysql.connect(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "",
+  });
+}
+connection.connect();
 module.exports = connection;
